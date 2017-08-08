dc40901d5b22d1c7f77ac59dd841cd249937ba94
ianoti <ianodhiambootieno@gmail.com>
2017-08-08 12:16:54 +0300

    binding functions and using forms in React

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cityTerm: '',
+    }
+
+    // this fixes the error of the "this" being referenced in the application being some mystery
+    // context and not one we are in control of and hence the undefined error crops up,
+    // we'll bind the action so it always opeartes in the right context
+    // this is a different way of fixing it instead of using a fat arrow function onChange={() => {this.onInputChange()}}
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ cityTerm: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render(){
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="get a 5 day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Search</button>
+        </span>
       </form>
     );
   }
